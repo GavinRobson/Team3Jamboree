fe18a5ffad9ee9f8c74be76785a114890363ac1f
Evan Digles <edigles1@students.towson.edu>
2024-11-14 16:28:28 -0500

    Added example enemy (Not in the game scene, but you can look at it.)

---
+++ components/game/classes/ExampleEnemy.js
@@ -0,0 +1,32 @@
+export default class ExampleEnemy extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture) {
+      super(scene, x, y, texture);
+  
+      scene.add.existing(this);
+  
+      scene.physics.add.existing(this);
+  
+      this.body.setCollideWorldBounds(true);
+  
+      this.speed = 40;
+      this.deadZone = 10;
+  
+      this.rotationSpeed = 5;
+   }
+    update(cursors, pointer) {
+      const distanceToCursor = Phaser.Math.Distance.Between(this.x, this.y, pointer.worldX, pointer.worldY);
+  
+      if (distanceToCursor > this.deadZone) {
+        const angleToCursor = Phaser.Math.Angle.Between(this.x, this.y, pointer.worldX, pointer.worldY);
+        this.scene.physics.moveTo(this, pointer.worldX, pointer.worldY, this.speed);
+  
+        this.rotation = Phaser.Math.Angle.RotateTo(this.rotation, angleToCursor, this.rotationSpeed);
+      } else {
+        this.body.setVelocity(0, 0);
+      }
+    }
+  
+    getAngle() {
+      return Phaser.Math.RadToDeg(this.rotation);
+    }
+  }
